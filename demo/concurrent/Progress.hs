08c93e90ea6ceb24e17c0f8bea8528ada2254d88
duncan_coutts <unknown>
2005-07-05 18:38:04 +0000

    [project @ 2005-07-05 18:38:04 by duncan_coutts] Use timeoutAddFull rather than idleAdd for the yield concurrency thing. This change means that it no longer takes 100% cpu time. By adjusting the timeout you can balance the latency and the polling load. A timeout value of 20 to 50 milliseconds seems to give adequete latency with negligable load. Also make the progress bar pulse more frequent.

--- demo/concurrent/Progress.hs
+++ demo/concurrent/Progress.hs
@@ -2,9 +2,9 @@
 --
 -- As long as GHC does not support OS level threads by default, a trick has
 -- to be used to let Haskell programs continue while the GUI is running.
--- We attach a call to the Haskell function "yield" to the idle handler of
--- Gtk's main loop. Thus, everytime Gtk is idleing, GHC gets a chance to
--- execute Haskell threads.
+-- We attach a call to the Haskell function "yield" to the timeout handler of
+-- Gtk's main loop. Thus GHC regularly gets a chance to execute Haskell
+-- threads.
 
 import Graphics.UI.Gtk
 
@@ -20,12 +20,16 @@ main = do
   boxPackStartDefaults contain pb
   widgetShowAll dia
   forkIO (doTask pb)
-  idleAdd (yield >> return True) priorityDefaultIdle
+
+  -- 50ms timeout, so GHC will get a chance to scheule about 20 times a second
+  -- which gives reasonable latency without the polling generating too much
+  -- cpu load.
+  timeoutAddFull (yield >> return True) priorityDefaultIdle 50
   dialogRun dia
   return ()
 
 doTask :: ProgressBar -> IO ()
 doTask pb = do
   progressBarPulse pb
-  threadDelay 1000000
+  threadDelay 100000
   doTask pb
