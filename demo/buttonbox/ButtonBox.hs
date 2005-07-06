1e6d06ff94a195f942384c2a19bce6a6415a55f0
duncan_coutts <unknown>
2005-07-06 13:13:27 +0000

    [project @ 2005-07-06 13:13:27 by duncan_coutts] Update the demo to use the "set" style syntax. Also show how to use child attributes.

--- demo/buttonbox/ButtonBox.hs
+++ demo/buttonbox/ButtonBox.hs
@@ -1,4 +1,4 @@
-module Main (Main.main) where
+module Main (main) where
 
 import Graphics.UI.Gtk
 
@@ -11,27 +11,39 @@ main = do
 
   -- Here we connect the "destroy" event to a signal handler.
   -- This event occurs when we call widgetDestroy on the window,
-  -- or if we return FALSE in the "delete_event" callback.
+  -- or if the user closes the window.
   window `onDestroy` mainQuit
 
   -- Sets the border width of the window.
-  containerSetBorderWidth window 10
+  set window [ containerBorderWidth := 10 ]
   
   hbuttonbox <- hButtonBoxNew
 
-  containerAdd window hbuttonbox
+  set window [ containerChild := hbuttonbox ]
 
   button1 <- buttonNewWithLabel "One"
   button2 <- buttonNewWithLabel "Two"
+  button3 <- buttonNewWithLabel "Three"
 
-  boxPackStart hbuttonbox button1 PackNatural 0
-  boxPackStart hbuttonbox button2 PackNatural 0
+  -- Add each button to the button box with the default packing and padding
+  set hbuttonbox [ containerChild := button
+                 | button <- [button1, button2, button3] ]
+  
+  -- This sets button3 to be a so called 'secondary child'. When the layout
+  -- stlye is ButtonboxStart or ButtonboxEnd, the secondary children are
+  -- grouped seperately from the others. Resize the window to see the effect.
+  --
+  -- This is not interesting in itself but shows how to set child attributes.
+  -- Note that the child attribute 'buttonBoxChildSecondary' takes the
+  -- button box container child 'button3' as a parameter.
+  set hbuttonbox [ buttonBoxLayoutStyle := ButtonboxStart
+		 , buttonBoxChildSecondary button3 := True ]
 
-  -- The final step is to display this newly created widgets
+  -- The final step is to display everything (the window and all the widgets
+  -- contained within it)
   widgetShowAll window
-  -- and the window
 
-  -- All GTK applications must have a Gtk.main. Control ends here and
+  -- All Gtk+ applications must run the main event loop. Control ends here and
   -- waits for an event to occur (like a key press or mouse event).
   mainGUI
 
