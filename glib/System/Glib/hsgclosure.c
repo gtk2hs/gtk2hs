e2019e30d5c8a4eae50db5caa6c2932a0c13b4a9
Duncan Coutts <duncan@coutts.uklinux.net>
2006-04-25 17:03:38 +0000

    #ifdef stuff to support the C FFI API of both ghc-6.4 and 6.5+

--- glib/System/Glib/hsgclosure.c
+++ glib/System/Glib/hsgclosure.c
@@ -40,7 +40,14 @@
  * do not need to replace these calls with anything else, so just define them
  * to expand to nothing: */
 #define rts_lock()
-#define rts_unlock()
+#define rts_unlock(x)
+#endif
+
+#if __GLASGOW_HASKELL__>604
+#define GHC_RTS_USES_CAPABILITY
+#define CAP cap,
+#else
+#define CAP
 #endif
 
 /* Gtk2HsClosure is a _private_ structure */
@@ -66,7 +73,11 @@ gtk2hs_closure_invalidate(gpointer data, GClosure *closure) {
 }
 
 /* forward defs */
+#ifdef GHC_RTS_USES_CAPABILITY
+static HaskellObj gtk2hs_value_as_haskellobj(Capability *cap, const GValue *value);
+#else
 static HaskellObj gtk2hs_value_as_haskellobj(const GValue *value);
+#endif
 static void gtk2hs_value_from_haskellobj(GValue *value, HaskellObj obj);
 
 extern StgClosure * GHCziStable_deRefStablePtr_closure;
@@ -82,11 +93,19 @@ gtk2hs_closure_marshal(GClosure *closure,
 
     Gtk2HsClosure *hc = (Gtk2HsClosure *)closure;
     HaskellObj call, ret;
-    SchedulerStatus rc;
+#ifdef GHC_RTS_USES_CAPABILITY
+    Capability *cap;
+#else
+    SchedulerStatus cap;
+#endif
     guint i;
     
     WHEN_DEBUG(g_debug("gtk2hs_closure_marshal: about to run callback=%p, n_param_values=%d", hc->callback, n_param_values));
+#ifdef GHC_RTS_USES_CAPABILITY
+    cap = rts_lock();
+#else
     rts_lock();
+#endif
     
     call = (StgClosure *)deRefStablePtr(hc->callback);
    
@@ -96,20 +115,20 @@ gtk2hs_closure_marshal(GClosure *closure,
                            i,
                            g_strdup_value_contents(&param_values[i]),
                            g_type_name(G_VALUE_TYPE(&param_values[i]))));
-        call = rts_apply(call, gtk2hs_value_as_haskellobj(&param_values[i]));
+        call = rts_apply(CAP call, gtk2hs_value_as_haskellobj(CAP &param_values[i]));
     }
     
     WHEN_DEBUG(g_debug("gtk2hs_closure_marshal: about to rts_evalIO"));
     
     /* perform the call */
-    rc=rts_evalIO(rts_apply((HaskellObj)runIO_closure, call),&ret);
+    cap=rts_evalIO(CAP rts_apply(CAP (HaskellObj)runIO_closure, call),&ret);
     
     WHEN_DEBUG(g_debug("gtk2hs_closure_marshal: about to rts_checkSchedStatus"));
     
     /* barf if anything went wrong */
     /* TODO: pass a sensible value for call site so we get better error messages */
     /* or perhaps we can propogate any error? */
-    rts_checkSchedStatus("gtk2hs_closure_marshal", rc);
+    rts_checkSchedStatus("gtk2hs_closure_marshal", cap);
     
     if (return_value) {
         WHEN_DEBUG(g_debug("gtk2hs_closure_marshal: return_value :: %s",
@@ -118,7 +137,7 @@ gtk2hs_closure_marshal(GClosure *closure,
         gtk2hs_value_from_haskellobj(return_value, ret);
     }
     
-    rts_unlock();
+    rts_unlock(CAP);
     WHEN_DEBUG(g_debug("gtk2hs_closure_marshal: done running callback"));
 }
 
@@ -143,49 +162,53 @@ gtk2hs_closure_new(HsStablePtr callback)
 /* GValue <-> HaskellObj marshaling functions */
 
 static HaskellObj
+#ifdef GHC_RTS_USES_CAPABILITY
+gtk2hs_value_as_haskellobj(Capability *cap, const GValue *value) {
+#else
 gtk2hs_value_as_haskellobj(const GValue *value) {
+#endif
     switch (G_TYPE_FUNDAMENTAL(G_VALUE_TYPE(value))) {
     case G_TYPE_INTERFACE:
         if (g_type_is_a(G_VALUE_TYPE(value), G_TYPE_OBJECT))
-            return rts_mkPtr(g_value_get_object(value));
+            return rts_mkPtr(CAP g_value_get_object(value));
         else
             break;
     case G_TYPE_CHAR:
-        return rts_mkChar(g_value_get_char(value));
+        return rts_mkChar(CAP g_value_get_char(value));
     case G_TYPE_UCHAR:
-        return rts_mkChar(g_value_get_uchar(value));
+        return rts_mkChar(CAP g_value_get_uchar(value));
     case G_TYPE_BOOLEAN:
-        return rts_mkBool(g_value_get_boolean(value));
+        return rts_mkBool(CAP g_value_get_boolean(value));
     case G_TYPE_INT:
-        return rts_mkInt(g_value_get_int(value));
+        return rts_mkInt(CAP g_value_get_int(value));
     case G_TYPE_UINT:
-        return rts_mkWord(g_value_get_uint(value));
+        return rts_mkWord(CAP g_value_get_uint(value));
     case G_TYPE_LONG:
-        return rts_mkInt(g_value_get_long(value));
+        return rts_mkInt(CAP g_value_get_long(value));
     case G_TYPE_ULONG:
-        return rts_mkWord(g_value_get_ulong(value));
+        return rts_mkWord(CAP g_value_get_ulong(value));
 /*    case G_TYPE_INT64:
-        return rts_mkInt64(g_value_get_int64(value));
+        return rts_mkInt64(CAP g_value_get_int64(value));
     case G_TYPE_UINT64:
-        return rts_mkWord64(g_value_get_uint64(value));   */
+        return rts_mkWord64(CAP g_value_get_uint64(value));   */
     case G_TYPE_ENUM:
-        return rts_mkInt(g_value_get_enum(value));
+        return rts_mkInt(CAP g_value_get_enum(value));
     case G_TYPE_FLAGS:
-        return rts_mkWord(g_value_get_enum(value));
+        return rts_mkWord(CAP g_value_get_enum(value));
     case G_TYPE_FLOAT:
-        return rts_mkFloat(g_value_get_float(value));
+        return rts_mkFloat(CAP g_value_get_float(value));
     case G_TYPE_DOUBLE:
-        return rts_mkDouble(g_value_get_double(value));
+        return rts_mkDouble(CAP g_value_get_double(value));
     case G_TYPE_STRING:
-        return rts_mkPtr((char *)g_value_get_string(value)); /* CHECKME: is the string freed? */
+        return rts_mkPtr(CAP (char *)g_value_get_string(value)); /* CHECKME: is the string freed? */
     case G_TYPE_POINTER:
-        return rts_mkPtr(g_value_get_pointer(value));
+        return rts_mkPtr(CAP g_value_get_pointer(value));
     case G_TYPE_BOXED:
-        return rts_mkPtr(g_value_get_boxed(value));
+        return rts_mkPtr(CAP g_value_get_boxed(value));
 /*    case G_TYPE_PARAM:
         return g_value_get_param(value); */
     case G_TYPE_OBJECT:
-        return rts_mkPtr(g_value_get_object(value));
+        return rts_mkPtr(CAP g_value_get_object(value));
     }
     g_error("gtk2hs_value_as_haskellobj: unable to handle GValue with type %s\n"
             "please report this as a bug to gtk2hs-devel@lists.sourceforge.net",
