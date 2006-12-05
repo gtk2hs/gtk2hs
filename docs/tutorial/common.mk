e8edaa0261b69ab122e440033eded0f1843cd6d8
Duncan Coutts <duncan@coutts.uklinux.net>
2006-12-05 19:25:44 +0000

    Add CSS file for tute docs For when not using a custom header/footer template ie this will be used by default

--- docs/tutorial/common.mk
+++ docs/tutorial/common.mk
@@ -23,13 +23,16 @@ $(TOP)/exports : $(hi-files)
 %.html : %.t2t $(TOP)/exports $(TOOLSDIR)/AddLinks
 	if [ -f $(TOP)/headder.html.fragment -a -f $(TOP)/footer.html.fragment ]; then \
 	  ( sed -e "s/%TITLE%/$$(head -n 1 $<)/" $(TOP)/headder.html.fragment; \
-	    txt2tags --target=xhtml --no-headers --infile=$< --outfile=- \
+	    txt2tags --target=xhtml --encoding utf-8 --no-headers \
+		--infile=$< --outfile=- \
 	    | $(TOOLSDIR)/AddLinks $(TOP)/exports --target=xhtml \
 		--baseurl=$(base-url); \
 	    cat $(TOP)/footer.html.fragment \
 	  ) > $@; \
 	else \
-	  txt2tags --target=xhtml --css-sugar --infile=$< --outfile=- \
+	  txt2tags --target=xhtml --encoding utf-8 \
+		--css-suggar --style=$(TOP)/gtk2hs-tutedocs.css \
+		--infile=$< --outfile=- \
 	  | $(TOOLSDIR)/AddLinks $(TOP)/exports --target=xhtml > $@; \
 	fi
 
