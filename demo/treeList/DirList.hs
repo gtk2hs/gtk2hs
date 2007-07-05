ea25f3be0da1139007fa4b58735a9ac59d27d5c4
Duncan Coutts <duncan@haskell.org>
2007-07-05 15:58:24 +0000

    Update DirList demo to work on Windows and include file modification times Thanks to Bulat Ziganshin for the patch. I also updated it to use multiple columns with titles and to avoid the use of unsafePerformIO by using the :=> monadic attribute assignment operator.

--- demo/treeList/DirList.hs
+++ demo/treeList/DirList.hs
@@ -4,12 +4,16 @@ module Main(main) where
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.ModelView as New
 
+import Control.Exception (handle)
 import System.Directory
-import System.Posix.Files
+import System.IO
+import System.Locale
+import System.Time
 
 data FileInfo = FileInfo {
   fName :: String,
-  fSize :: Integer
+  fSize :: Integer,
+  fTime :: ClockTime
 }
 
 main = do
@@ -20,27 +24,53 @@ main = do
   curDir <- getCurrentDirectory
   files <- getDirectoryContents curDir
   fInfos <- (flip mapM) files $ \f -> do
-    status <- getFileStatus f
-    return FileInfo { fName = f,
-		      fSize = fromIntegral (fileSize status) }
+    s <- handle (\_ -> return 0) $
+           do h <- openFile f ReadMode
+              s <- hFileSize h
+              hClose h
+              return s
+    t <- getModificationTime f
+    return FileInfo { fName = f
+		    , fSize = s
+		    , fTime = t }
 
   store <- New.listStoreNew fInfos
 
   tv <- New.treeViewNewWithModel store
-  win `containerAdd` tv
+  containerAdd win tv
 
   tvc <- New.treeViewColumnNew
+  set tvc [ New.treeViewColumnTitle := "File name"
+          , New.treeViewColumnResizable := True ]
   New.treeViewAppendColumn tv tvc
 
-  text <- New.cellRendererTextNew
-  New.treeViewColumnPackStart tvc text True
-  New.cellLayoutSetAttributes tvc text store
-    (\FileInfo { fName = name } -> [New.cellText := name])
+  name <- New.cellRendererTextNew
+  New.treeViewColumnPackStart tvc name True
+  New.cellLayoutSetAttributes tvc name store $ \FileInfo { fName = name } ->
+    [ New.cellText := name ]
+
+  tvc <- New.treeViewColumnNew
+  set tvc [ New.treeViewColumnTitle := "Size"
+          , New.treeViewColumnResizable := True ]
+  New.treeViewAppendColumn tv tvc
 
   size <- New.cellRendererTextNew
   New.treeViewColumnPackStart tvc size True
-  New.cellLayoutSetAttributes tvc size store
-    (\FileInfo { fSize = size } -> [New.cellText := show size])
+  New.cellLayoutSetAttributes tvc size store $ \FileInfo { fSize = size } ->
+    [ New.cellText := show size ]
+
+  tvc <- New.treeViewColumnNew
+  set tvc [ New.treeViewColumnTitle := "Modification time"
+          , New.treeViewColumnResizable := True ]
+  New.treeViewAppendColumn tv tvc
+
+  time <- New.cellRendererTextNew
+  New.treeViewColumnPackStart tvc time True
+  New.cellLayoutSetAttributes tvc time store $ \FileInfo { fTime = time } ->
+    [ New.cellText :=> do
+        calTime <- toCalendarTime time
+        return (formatCalendarTime defaultTimeLocale "%D %T" calTime)
+    ]
 
   widgetShowAll win
   mainGUI
