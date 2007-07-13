a9ab306291fab8acec81ec16ceef44e89b918eeb
Peter Gavin <pgavin@gmail.com>
2007-07-13 17:56:25 +0000

    glib: add GDate to exports in Glib.hs

--- glib/System/Glib.hs
+++ glib/System/Glib.hs
@@ -6,7 +6,8 @@ module System.Glib (
   module System.Glib.GObject,
   module System.Glib.GParameter,
   module System.Glib.GError,
-  module System.Glib.GList
+  module System.Glib.GList,
+  module System.Glib.GDate
   ) where
 
 import System.Glib.UTFString
@@ -16,4 +17,5 @@ import System.Glib.GObject
 import System.Glib.GParameter
 import System.Glib.GError
 import System.Glib.GList
+import System.Glib.GDate
 -- do not import System.Glib.FFI
