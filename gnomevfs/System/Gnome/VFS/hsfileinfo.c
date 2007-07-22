7345c5580b1a250cab97bcec49b68b5a36ce9cf0
Peter Gavin <pgavin@gmail.com>
2007-07-22 02:46:18 +0000

    gnomevfs: updated license to LGPLv3, made all source file headers consistent

--- gnomevfs/System/Gnome/VFS/hsfileinfo.c
+++ gnomevfs/System/Gnome/VFS/hsfileinfo.c
@@ -1,3 +1,32 @@
+/* GIMP Toolkit (GTK) Binding for Haskell: binding to libgnomevfs -*-c-*-
+ *
+ * Author : Peter Gavin
+ * Created: 1-Apr-2007
+ *
+ * Copyright (c) 2007 Peter Gavin
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * GnomeVFS, the C library which this Haskell library depends on, is
+ * available under LGPL Version 2. The documentation included with
+ * this library is based on the original GnomeVFS documentation,
+ * Copyright (c) 2001 Seth Nickell <snickell@stanford.edu>. The
+ * documentation is covered by the GNU Free Documentation License,
+ * version 1.2.
+ */
+
 #include "hsfileinfo.h"
 
 GnomeVFSFileType
