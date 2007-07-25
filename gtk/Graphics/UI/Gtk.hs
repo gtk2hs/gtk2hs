1f9eada1bd8556c7bd1c16ab5f2fb31d23fa64dd
Andrea Vezzosi <sanzhiyan@gmail.com>
2007-07-25 03:34:33 +0000

    Add GtkStatusIcon bindings

--- gtk/Graphics/UI/Gtk.hs
+++ gtk/Graphics/UI/Gtk.hs
@@ -70,6 +70,7 @@ module Graphics.UI.Gtk (
   module Graphics.UI.Gtk.Display.Label,
   module Graphics.UI.Gtk.Display.ProgressBar,
   module Graphics.UI.Gtk.Display.Statusbar,
+  module Graphics.UI.Gtk.Display.StatusIcon,
   -- * Buttons and toggles
   module Graphics.UI.Gtk.Buttons.Button,
   module Graphics.UI.Gtk.Buttons.CheckButton,
@@ -245,6 +246,7 @@ import Graphics.UI.Gtk.Display.Image
 import Graphics.UI.Gtk.Display.Label
 import Graphics.UI.Gtk.Display.ProgressBar
 import Graphics.UI.Gtk.Display.Statusbar
+import Graphics.UI.Gtk.Display.StatusIcon hiding (onActivate,afterActivate,onPopupMenu,afterPopupMenu)
 -- buttons and toggles
 import Graphics.UI.Gtk.Buttons.Button
 import Graphics.UI.Gtk.Buttons.CheckButton
