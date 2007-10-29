0120d205b5fa2d4551da70b5fe0869775d494abc
Peter Gavin <pgavin@gmail.com>
2007-10-29 16:07:49 +0000

    gstreamer: remove M.S.G.Core.Buffer.bufferWithDataM I need to rethink this one

--- gstreamer/Media/Streaming/GStreamer/Core/Buffer.chs.pp
+++ gstreamer/Media/Streaming/GStreamer/Core/Buffer.chs.pp
@@ -44,7 +44,6 @@ module Media.Streaming.GStreamer.Core.Buffer (
   bufferGetData,
   bufferGetDataM,
   bufferSetDataM,
-  bufferWithDataM,
   bufferGetTimestamp,
   bufferGetTimestampM,
   bufferSetTimestampM,
@@ -171,24 +170,6 @@ bufferSetDataM bs =
                {# set GstBuffer->data #} bufferPtr newData
                {# set GstBuffer->malloc_data #} bufferPtr newData
                {# set GstBuffer->size #} bufferPtr $ fromIntegral size
-
-bufferWithDataM :: BufferClass bufferT
-                => (BS.ByteString -> MiniObjectM bufferT a)
-                -> MiniObjectM bufferT a
-bufferWithDataM action =
-    do bs <- marshalBufferM $ \bufferPtr ->
-                 do ptr <- {# get GstBuffer->data #} bufferPtr
-                    size <- {# get GstBuffer->size #} bufferPtr
-#if __GLASGOW_HASKELL__ < 608
-                    return $ BS.packCStringLen (castPtr ptr, fromIntegral size)
-#else
-                    BS.packCStringLen (castPtr ptr, fromIntegral size)
-#endif
-       result <- action bs
-       MiniObjectM $ \buffer ->
-           let (Buffer bufferFPtr) = toBuffer buffer
-           in touchForeignPtr bufferFPtr
-       return result
 
 bufferGetTimestamp :: BufferClass bufferT
                    => bufferT
