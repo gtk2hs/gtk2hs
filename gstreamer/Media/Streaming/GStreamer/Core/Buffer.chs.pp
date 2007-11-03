1020f4513588cbd07170237ac40075c7e962f3da
Peter Gavin <pgavin@gmail.com>
2007-11-03 13:55:04 +0000

    gstreamer: M.S.G.Core.Buffer: add documentation, API improvements

--- gstreamer/Media/Streaming/GStreamer/Core/Buffer.chs.pp
+++ gstreamer/Media/Streaming/GStreamer/Core/Buffer.chs.pp
@@ -26,14 +26,29 @@
 -- | Maintainer  : gtk2hs-devel@lists.sourceforge.net
 --   Stability   : alpha
 --   Portability : portable (depends on GHC)
+--
+--   Data-passing buffer type, supporting sub-buffers.
 module Media.Streaming.GStreamer.Core.Buffer (
   
+-- * Types
+
+  -- | Buffers are the basic unit of data transfer in GStreamer. The
+  --   'Buffer' type provides all the state necessary to define a
+  --   region of memory as part of a stream. Sub-buffers are also
+  --   supported, allowing a smaller region of a buffer to become its
+  --   own buffer, with mechansims in place to ensure that neither
+  --   memory space goes away prematurely.
   Buffer,
   BufferClass,
   BufferFlags(..),
-  toBuffer,
+  -- Safely downcast an 'Object' to a 'Bin'.
   castToBuffer,
+  -- Upcast to a 'Bin'.
+  toBuffer,
+  -- See if an 'Object' is a 'Bin'.
   isBuffer,
+
+-- * Buffer Operations
   bufferOffsetNone,
   bufferGetFlags,
   bufferGetFlagsM,
@@ -44,6 +59,7 @@ module Media.Streaming.GStreamer.Core.Buffer (
   bufferGetData,
   bufferGetDataM,
   bufferSetDataM,
+  unsafeBufferGetPtrM,
   bufferGetTimestamp,
   bufferGetTimestampM,
   bufferSetTimestampM,
@@ -59,14 +75,6 @@ module Media.Streaming.GStreamer.Core.Buffer (
   bufferGetOffsetEnd,
   bufferGetOffsetEndM,
   bufferSetOffsetEndM,
-  bufferDurationIsValid,
-  bufferDurationIsValidM,
-  bufferTimestampIsValid,
-  bufferTimestampIsValidM,
-  bufferOffsetIsValid,
-  bufferOffsetIsValidM,
-  bufferOffsetEndIsValid,
-  bufferOffsetEndIsValidM,
   bufferIsDiscont,
   bufferIsDiscontM,
   
@@ -76,7 +84,6 @@ module Media.Streaming.GStreamer.Core.Buffer (
   
   bufferIsSpanFast,
   bufferSpan,
-  bufferStampM,
   bufferMerge
   
   ) where
@@ -94,26 +101,30 @@ import System.Glib.FFI
 
 {# context lib = "gstreamer" prefix = "gst" #}
 
-
+-- | Get the flags set on this buffer.
 bufferGetFlags :: BufferClass bufferT
                => bufferT
                -> [BufferFlags]
 bufferGetFlags = mkMiniObjectGetFlags
 
+-- | Get the flags set on the current buffer.
 bufferGetFlagsM :: BufferClass bufferT
                 => MiniObjectM bufferT [BufferFlags]
 bufferGetFlagsM = mkMiniObjectGetFlagsM
 
+-- | Set flags on the current buffer.
 bufferSetFlagsM :: BufferClass bufferT
                 => [BufferFlags]
                 -> MiniObjectM bufferT () 
 bufferSetFlagsM = mkMiniObjectSetFlagsM
 
+-- | Unset flags on the current buffer.
 bufferUnsetFlagsM :: BufferClass bufferT
                   => [BufferFlags]
                   -> MiniObjectM bufferT ()
 bufferUnsetFlagsM = mkMiniObjectUnsetFlagsM
 
+-- | Get the size of the buffer.
 bufferGetSize :: BufferClass bufferT
               => bufferT
               -> Word
@@ -127,12 +138,14 @@ marshalBufferM :: BufferClass bufferT
 marshalBufferM action =
     MiniObjectM $ flip (withMiniObject . toBuffer) $ action
 
+-- | Get the size of the current buffer.
 bufferGetSizeM :: BufferClass bufferT
                => MiniObjectM bufferT Word
 bufferGetSizeM =
     liftM fromIntegral $
         marshalBufferM {# get GstBuffer->size #}
 
+-- | Make an O(n) copy of the buffer.
 bufferGetData :: BufferClass bufferT
               => bufferT
               -> BS.ByteString
@@ -146,6 +159,7 @@ bufferGetData buffer =
            BS.packCStringLen (castPtr ptr, fromIntegral size)
 #endif
 
+-- | Make an O(n) copy of the current buffer.
 bufferGetDataM :: BufferClass bufferT
                => MiniObjectM bufferT BS.ByteString
 bufferGetDataM =
@@ -158,6 +172,7 @@ bufferGetDataM =
            BS.packCStringLen (castPtr ptr, fromIntegral size)
 #endif
 
+-- | Store an O(n) copy of the provided data in the current buffer.
 bufferSetDataM :: BufferClass bufferT
                => BS.ByteString
                -> MiniObjectM bufferT ()
@@ -173,45 +188,93 @@ bufferSetDataM bs =
                {# set GstBuffer->malloc_data #} bufferPtr newData
                {# set GstBuffer->size #} bufferPtr $ fromIntegral size
 
+-- | Get a raw pointer to the internal data area for the current
+--   buffer. The pointer may be used to write into the data area if
+--   desired. This function is unsafe in that the pointer should not
+--   be used once the buffer is returned.
+unsafeBufferGetPtrM :: BufferClass bufferT
+                    => MiniObjectM bufferT (Ptr Word8)
+unsafeBufferGetPtrM =
+    MiniObjectM $ \buffer ->
+        liftM castPtr $
+            withMiniObject buffer $
+                {# get GstBuffer->data #} . castPtr
+
+marshalGetNum :: (BufferClass bufferT, Integral intT, Num numT)
+              => (Ptr Buffer -> IO intT)
+              -> numT
+              -> bufferT
+              -> Maybe numT
+marshalGetNum getAction invalid buffer =
+    let n = fromIntegral $ unsafePerformIO $
+                withMiniObject (toBuffer buffer) getAction
+    in if n /= invalid
+          then Just n
+          else Nothing
+
+marshalGetNumM :: (BufferClass bufferT, Integral intT, Num numT)
+              => (Ptr Buffer -> IO intT)
+              -> numT
+              -> MiniObjectM bufferT (Maybe numT)
+marshalGetNumM getAction invalid =
+    marshalBufferM $ \bufferPtr -> do
+      n <- liftM fromIntegral $ getAction bufferPtr
+      return $ if n /= invalid
+                  then Just n
+                  else Nothing
+
+marshalSetNumM :: (BufferClass bufferT, Integral intT, Num numT)
+               => (Ptr Buffer -> numT -> IO ())
+               -> intT
+               -> Maybe intT
+               -> MiniObjectM bufferT ()
+marshalSetNumM setAction invalid nM =
+    let n = case nM of
+              Just n' -> n'
+              Nothing -> invalid
+    in marshalBufferM $ flip setAction $ fromIntegral n
+
+-- | Get the timestamp for this buffer.
 bufferGetTimestamp :: BufferClass bufferT
                    => bufferT
-                   -> ClockTime
-bufferGetTimestamp buffer =
-    fromIntegral $ unsafePerformIO $
-        withMiniObject buffer {# get GstBuffer->timestamp #}
+                   -> Maybe ClockTime
+bufferGetTimestamp =
+    marshalGetNum {# get GstBuffer->timestamp #} clockTimeNone
 
+-- | Get the timestamp of the current buffer.
 bufferGetTimestampM :: BufferClass bufferT
-                    => MiniObjectM bufferT ClockTime
+                    => MiniObjectM bufferT (Maybe ClockTime)
 bufferGetTimestampM =
-    liftM fromIntegral $
-        marshalBufferM {# get GstBuffer->timestamp #}
+    marshalGetNumM {# get GstBuffer->timestamp #} clockTimeNone
 
+-- | Set the timestamp of the current buffer.
 bufferSetTimestampM :: BufferClass bufferT
-                    => ClockTime
+                    => Maybe ClockTime
                     -> MiniObjectM bufferT ()
-bufferSetTimestampM timestamp =
-    marshalBufferM $ flip {# set GstBuffer->timestamp #} $ fromIntegral timestamp
+bufferSetTimestampM =
+    marshalSetNumM {# set GstBuffer->timestamp #} clockTimeNone
 
+-- | Get the duration of the buffer.
 bufferGetDuration :: BufferClass bufferT
                   => bufferT
-                  -> ClockTime
-bufferGetDuration buffer =
-    fromIntegral $ unsafePerformIO $
-        withMiniObject buffer {# get GstBuffer->duration #}
+                  -> Maybe ClockTime
+bufferGetDuration =
+    marshalGetNum {# get GstBuffer->duration #} clockTimeNone
 
+-- | Get the duration of the current buffer.
 bufferGetDurationM :: BufferClass bufferT
-                   => MiniObjectM bufferT ClockTime
+                   => MiniObjectM bufferT (Maybe ClockTime)
 bufferGetDurationM =
-    liftM fromIntegral $
-        marshalBufferM {# get GstBuffer->duration #}
+    marshalGetNumM {# get GstBuffer->duration #} clockTimeNone
 
+-- | Set the duration of the current buffer.
 bufferSetDurationM :: BufferClass bufferT
-                   => ClockTime
+                   => Maybe ClockTime
                    -> MiniObjectM bufferT ()
-bufferSetDurationM duration =
-    marshalBufferM $
-        flip {# set GstBuffer->duration #} $ fromIntegral duration
+bufferSetDurationM =
+    marshalSetNumM {# set GstBuffer->duration #} clockTimeNone
 
+-- | Get the caps of the buffer.
 bufferGetCaps :: BufferClass bufferT
               => bufferT
               -> Maybe Caps
@@ -220,6 +283,7 @@ bufferGetCaps buffer =
         {# call buffer_get_caps #} (toBuffer buffer) >>=
             maybePeek takeCaps
 
+-- | Get the caps of the current buffer.
 bufferGetCapsM :: BufferClass bufferT
                => MiniObjectM bufferT (Maybe Caps)
 bufferGetCapsM =
@@ -227,132 +291,108 @@ bufferGetCapsM =
         {# call buffer_get_caps #} (toBuffer buffer) >>=
             maybePeek takeCaps
 
+-- | Set the caps of the current buffer.
 bufferSetCapsM :: BufferClass bufferT
-               => Caps
+               => Maybe Caps
                -> MiniObjectM bufferT ()
-bufferSetCapsM caps =
+bufferSetCapsM capsM =
     MiniObjectM $ \buffer ->
-        {# call buffer_set_caps #} (toBuffer buffer) caps
+        {# call buffer_set_caps #} (toBuffer buffer) $
+            case capsM of
+              Just caps -> caps
+              Nothing   -> Caps nullForeignPtr
 
+-- | Get the offset of the buffer.
 bufferGetOffset :: BufferClass bufferT
                 => bufferT
-                -> Word64
-bufferGetOffset buffer =
-    fromIntegral $ unsafePerformIO $
-        withMiniObject buffer {# get GstBuffer->offset #}
+                -> Maybe Word64
+bufferGetOffset =
+    marshalGetNum {# get GstBuffer->offset #} bufferOffsetNone
 
+-- | Get the start offset of the current buffer.
 bufferGetOffsetM :: BufferClass bufferT
-                 => MiniObjectM bufferT Word64
+                 => MiniObjectM bufferT (Maybe Word64)
 bufferGetOffsetM =
-    liftM fromIntegral $
-        marshalBufferM {# get GstBuffer->offset #}
+    marshalGetNumM {# get GstBuffer->offset #} bufferOffsetNone
 
+-- | Set the start offset of the current buffer.
 bufferSetOffsetM :: BufferClass bufferT
-                 => Word64
+                 => Maybe Word64
                  -> MiniObjectM bufferT ()
-bufferSetOffsetM offset =
-    marshalBufferM $
-        flip {# set GstBuffer->offset #} $ fromIntegral offset
+bufferSetOffsetM =
+    marshalSetNumM {# set GstBuffer->offset #} bufferOffsetNone
 
+-- | Get the end offset of the buffer.
 bufferGetOffsetEnd :: BufferClass bufferT
                    => bufferT
-                   -> Word64
-bufferGetOffsetEnd buffer =
-    fromIntegral $ unsafePerformIO $
-        withMiniObject buffer {# get GstBuffer->offset_end #}
+                   -> Maybe Word64
+bufferGetOffsetEnd =
+    marshalGetNum {# get GstBuffer->offset_end #} bufferOffsetNone
 
+-- | Get the end offset of the current buffer.
 bufferGetOffsetEndM :: BufferClass bufferT
-                    => MiniObjectM bufferT Word64
+                    => MiniObjectM bufferT (Maybe Word64)
 bufferGetOffsetEndM =
-    liftM fromIntegral $
-        marshalBufferM {# get GstBuffer->offset_end #}
+    marshalGetNumM {# get GstBuffer->offset_end #} bufferOffsetNone
 
+-- | Set the end offset of the current buffer.
 bufferSetOffsetEndM :: BufferClass bufferT
-                    => Word64
+                    => Maybe Word64
                     -> MiniObjectM bufferT ()
-bufferSetOffsetEndM offsetEnd =
-    marshalBufferM $
-        flip {# set GstBuffer->offset_end #} $ fromIntegral offsetEnd
+bufferSetOffsetEndM =
+    marshalSetNumM {# set GstBuffer->offset_end #} bufferOffsetNone
 
-bufferDurationIsValid :: BufferClass bufferT
-                      => bufferT
-                      -> Bool
-bufferDurationIsValid =
-    (/= clockTimeNone) . bufferGetOffset
-
-bufferDurationIsValidM :: BufferClass bufferT
-                       => MiniObjectM bufferT Bool
-bufferDurationIsValidM =
-    liftM (/= clockTimeNone) $ bufferGetDurationM
-
-bufferTimestampIsValid :: BufferClass bufferT
-                       => bufferT
-                       -> Bool
-bufferTimestampIsValid =
-    (/= clockTimeNone) . bufferGetTimestamp
-
-bufferTimestampIsValidM :: BufferClass bufferT
-                        => MiniObjectM bufferT Bool
-bufferTimestampIsValidM =
-    liftM (/= clockTimeNone) $ bufferGetTimestampM
-
-bufferOffsetIsValid :: BufferClass bufferT
-                    => bufferT
-                    -> Bool
-bufferOffsetIsValid =
-    (/= bufferOffsetNone) . bufferGetOffset
-
-bufferOffsetIsValidM :: BufferClass bufferT
-                     => MiniObjectM bufferT Bool
-bufferOffsetIsValidM =
-    liftM (/= bufferOffsetNone) $ bufferGetOffsetM
-
-bufferOffsetEndIsValid :: BufferClass bufferT
-                       => bufferT
-                       -> Bool
-bufferOffsetEndIsValid =
-    (/= bufferOffsetNone) . bufferGetOffsetEnd
-
-bufferOffsetEndIsValidM :: BufferClass bufferT
-                        => MiniObjectM bufferT Bool
-bufferOffsetEndIsValidM =
-    liftM (/= bufferOffsetNone) $ bufferGetOffsetEndM
-
+-- | Return 'True' if the buffer marks a discontinuity in a stream, or
+--   'False' otherwise. This typically occurs after a seek or a
+--   dropped buffer from a live or network source.
 bufferIsDiscont :: BufferClass bufferT
                 => bufferT
                 -> Bool
 bufferIsDiscont =
     (elem BufferDiscont) . bufferGetFlags
 
+-- | Return 'True' if the current buffer marks a discontinuity in a
+--   stream, or 'False' otherwise.
 bufferIsDiscontM :: BufferClass bufferT
                  => MiniObjectM bufferT Bool
 bufferIsDiscontM =
     liftM (elem BufferDiscont) $ bufferGetFlagsM
 
-bufferCreateEmpty :: MiniObjectM Buffer a
-                  -> IO (Buffer, a)
+-- | Create an empty buffer and mutates it according to the given
+--   action. Once this function returns, the buffer is immutable.
+bufferCreateEmpty :: MiniObjectM Buffer a -- ^ the mutating action
+                  -> IO (Buffer, a)       -- ^ the new buffer and the action's result
 bufferCreateEmpty =
     marshalMiniObjectModify {# call buffer_new #}
 
-bufferCreate :: Word
-             -> MiniObjectM Buffer a
-             -> IO (Buffer, a)
+-- | Create and mutate a buffer of the given size.
+bufferCreate :: Word                 -- ^ the size of the buffer to be created
+             -> MiniObjectM Buffer a -- ^ the mutating action
+             -> IO (Buffer, a)       -- ^ the new buffer and the action's result
 bufferCreate size =
     marshalMiniObjectModify $
         {# call buffer_new_and_alloc #} $ fromIntegral size
 
+-- | Create a sub-buffer from an existing buffer with the given offset
+--   and size. This sub-buffer uses the actual memory space of the
+--   parent buffer. Thus function will copy the offset and timestamp
+--   fields when the offset is 0. Otherwise, they will both be set to
+--   'Nothing'. If the offset is 0 and the size is the total size of
+--   the parent, the duration and offset end fields are also
+--   copied. Otherwise they will be set to 'Nothing'.
 bufferCreateSub :: BufferClass bufferT
-                => bufferT
-                -> Word
-                -> Word
-                -> MiniObjectM Buffer a
-                -> IO (Buffer, a)
+                => bufferT           -- ^ the parent buffer
+                -> Word              -- ^ the offset
+                -> Word              -- ^ the size
+                -> IO (Maybe Buffer) -- ^ the new sub-buffer
 bufferCreateSub parent offset size =
-    marshalMiniObjectModify $
-        {# call buffer_create_sub #} (toBuffer parent)
-                                     (fromIntegral offset)
-                                     (fromIntegral size)
+    {# call buffer_create_sub #} (toBuffer parent)
+                                 (fromIntegral offset)
+                                 (fromIntegral size) >>=
+        maybePeek takeMiniObject
 
+-- | Return 'True' if 'bufferSpan' can be done without copying the
+--   data, or 'False' otherwise.
 bufferIsSpanFast :: (BufferClass bufferT1, BufferClass bufferT2)
                  => bufferT1
                  -> bufferT2
@@ -362,12 +402,21 @@ bufferIsSpanFast buffer1 buffer2 =
         {# call buffer_is_span_fast #} (toBuffer buffer1)
                                        (toBuffer buffer2)
 
+-- | Create a new buffer that consists of a span across the given
+--   buffers. Logically, the buffers are concatenated to make a larger
+--   buffer, and a new buffer is created at the given offset and with
+--   the given size.
+--   
+--   If the two buffers are children of the same larger buffer, and
+--   are contiguous, no copying is necessary. You can use
+--   'bufferIsSpanFast' to determine if copying is needed.
 bufferSpan :: (BufferClass bufferT1, BufferClass bufferT2)
-           => bufferT1
-           -> Word32
-           -> bufferT2
-           -> Word32
-           -> Maybe Buffer
+           => bufferT1     -- ^ the first buffer
+           -> Word32       -- ^ the offset into the concatenated buffer
+           -> bufferT2     -- ^ the second buffer
+           -> Word32       -- ^ the length of the final buffer
+           -> Maybe Buffer -- ^ the spanning buffer, or 'Nothing' if
+                           --   the arguments are invalid
 bufferSpan buffer1 offset buffer2 len =
     unsafePerformIO $
         {# call buffer_span #} (toBuffer buffer1)
@@ -376,18 +425,12 @@ bufferSpan buffer1 offset buffer2 len =
                                (fromIntegral len) >>=
             maybePeek takeMiniObject
 
-bufferStampM :: (BufferClass srcT, BufferClass destT)
-             => srcT
-             -> MiniObjectM destT ()
-bufferStampM src =
-    MiniObjectM $ \dest ->
-        {# call buffer_stamp #} (toBuffer dest)
-                                (toBuffer src)
-
+-- | Concatenate two buffers. If the buffers point to contiguous memory
+--   areas, no copying will occur.
 bufferMerge :: (BufferClass bufferT1, BufferClass bufferT2)
-            => bufferT1
-            -> bufferT2
-            -> Buffer
+            => bufferT1 -- ^ a buffer
+            -> bufferT2 -- ^ a buffer
+            -> Buffer   -- ^ the concatenation of the buffers
 bufferMerge buffer1 buffer2 =
     unsafePerformIO $
         {# call buffer_merge #} (toBuffer buffer1)
