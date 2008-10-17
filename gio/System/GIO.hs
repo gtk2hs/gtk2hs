c6c34376dff5cf50ae612dd1fc7f0485c0c2f33f
Peter Gavin <pgavin@gmail.com>
2008-10-17 16:48:29 +0000

    gio: add module System.GIO

---
+++ gio/System/GIO.hs
@@ -0,0 +1,41 @@
+--  GIMP Toolkit (GTK) Binding for Haskell: binding to gio -*-haskell-*-
+--
+--  Author : Peter Gavin
+--  Created: 13-Oct-2008
+--
+--  Copyright (c) 2008 Peter Gavin
+--
+--  This library is free software: you can redistribute it and/or
+--  modify it under the terms of the GNU Lesser General Public License
+--  as published by the Free Software Foundation, either version 3 of
+--  the License, or (at your option) any later version.
+--  
+--  This library is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--  Lesser General Public License for more details.
+--  
+--  You should have received a copy of the GNU Lesser General Public
+--  License along with this program.  If not, see
+--  <http://www.gnu.org/licenses/>.
+--  
+--  GIO, the C library which this Haskell library depends on, is
+--  available under LGPL Version 2. The documentation included with
+--  this library is based on the original GIO documentation,
+--  Copyright (c) 2001 Seth Nickell <snickell@stanford.edu>. The
+--  documentation is covered by the GNU Free Documentation License,
+--  version 1.2.
+--  
+-- | Maintainer  : gtk2hs-devel@lists.sourceforge.net
+--   Stability   : alpha
+--   Portability : portable (depends on GHC)
+module System.GIO (
+    module System.GIO.AsyncResult,
+    module System.GIO.Cancellable,
+    module System.GIO.File
+    ) where
+
+import System.GIO.AsyncResult
+import System.GIO.Cancellable
+import System.GIO.File
+
