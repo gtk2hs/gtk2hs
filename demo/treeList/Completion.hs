629ef885cd2920bfd0d8e46f9bc4475ee78655a2
Axel.Simon <Axel.Simon@ens.fr>
2008-10-27 19:59:37 +0000

    Make the completion demo much more funky.

--- demo/treeList/Completion.hs
+++ demo/treeList/Completion.hs
@@ -1,28 +1,82 @@
+-- Demo to show off entry completion.
+
 import Graphics.UI.Gtk
-import qualified Graphics.UI.Gtk.ModelView as New
+import Data.Char
+import Data.List
 
--- Define a column number to hold a string (the first type argument of
--- ColumnId is the type stored in the model which just happens to be a string
--- too). Each model has a map from column numbers to extraction functions.
--- This function is set below using 'treeModelSetColumn'. Column numbers can
--- be any number greater or equal to 0, but they should be low as the stores
--- create a list of extraction functions starting with column zero.
-strCol :: New.ColumnId String String
-strCol = New.makeColumnIdString 1
+data ColorDesc = ColorDesc {
+ cdColor :: Color,
+ cdName :: String
+ } deriving Show
 
-main = 
+compareCol :: ColumnId ColorDesc String
+compareCol = makeColumnIdString 0
+
+invertColor (Color r g b) = Color (32767+r) (32767+g) (32767+b) 
+--Color (65535-r) (65535-g) (65535-b)
+
+parseColor s = ColorDesc c (dropWhile isSpace (upperToSpace name))
+  where
+  s1 = dropWhile isSpace s
+  (s2,s3) = span isDigit s1
+  s4 = dropWhile isSpace s3
+  (s5,s6) = span isDigit s4
+  s7 = dropWhile isSpace s6
+  (s8,s9) = span isDigit s7
+  n1 = read ('0':s2)
+  n2 = read ('0':s5)
+  n3 = read ('0':s8)
+  c = Color (n1*256+n1) (n2*256+n2) (n3*256+n3)
+  name = dropWhile isSpace s9
+  upperToSpace [] = []
+  upperToSpace (x:xs) | isUpper x = ' ':toLower x:upperToSpace xs
+                      | otherwise = x:upperToSpace xs
+
+main =
     do
       initGUI
       window <- windowNew
-      store <- New.listStoreNew ["red","green","magenta"]
+      
+      contents <- readFile "rgb.txt"
+      let killDups [] = []
+          killDups [x] = [x]
+          killDups (x:y:xs) | cdName x==cdName y = killDups (y:xs)
+                            | otherwise = x:killDups (y:xs)
+          cols = killDups $ map parseColor (drop 1 (lines contents))
+      store <- listStoreNew cols
+      customStoreSetColumn store compareCol cdName
+
       entry <- entryNew
-      completion <- New.entryCompletionNew
-      -- Define the extraction function for this column to be the identity.
-      New.treeModelSetColumn store strCol id
-      set completion [entryCompletionModel := Just store,
-                      entryCompletionTextColumn := strCol]
+      completion <- entryCompletionNew
       entrySetCompletion entry completion
+
+      set completion [entryCompletionModel := Just store]
+      cell <- cellRendererTextNew
+      set cell [cellTextBackgroundSet := True,
+                cellTextForegroundSet := True]
+      cellLayoutPackStart completion cell True
+      cellLayoutSetAttributes completion cell store
+        (\cd -> [cellText := cdName cd,
+                 cellTextBackgroundColor := cdColor cd,
+                 cellTextForegroundColor := invertColor (cdColor cd)])
+      entryCompletionSetMatchFunc completion (matchFunc store)
+      on completion matchSelected $ \model iter -> do
+        color <- treeModelGetValue model iter compareCol
+        entrySetText entry color
+        return True
       set window [containerChild := entry]
       widgetShowAll window
       onDestroy window mainQuit
       mainGUI
+
+matchFunc :: ListStore ColorDesc -> String -> TreeIter -> IO Bool
+matchFunc model str iter = do
+  --putStrLn ("iter is "++show iter)
+  tp <- treeModelGetPath model iter
+  r <- case tp of
+         (i:_) -> do row <- listStoreGetValue model i
+                     return $ any (isPrefixOf (map toLower str))
+                                  (words (map toLower (cdName row)))
+         otherwise -> return False
+  return r
+
