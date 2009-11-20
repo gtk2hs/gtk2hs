840da090e6222cd2a8d9f3780ff422e960148e64
John Millikin <jmillikin@gmail.com>
2009-11-20 18:39:43 +0000

    Add a demo for GtkBuilder.

---
+++ demo/gtkbuilder/GtkBuilderTest.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Graphics.UI.Gtk
+
+main = do
+	initGUI
+	
+	-- Create the builder, and load the UI file
+	builder <- builderNew
+	builderAddFromFile builder "simple.ui"
+	
+	-- Retrieve some objects from the UI
+	window <- builderGetObject builder castToWindow "window1"
+	button <- builderGetObject builder castToButton "button1"
+	
+	-- Basic user interation
+	button `onClicked` putStrLn "button pressed!"
+	window `onDestroy` mainQuit
+	
+	-- Display the window
+	widgetShowAll window
+	mainGUI
