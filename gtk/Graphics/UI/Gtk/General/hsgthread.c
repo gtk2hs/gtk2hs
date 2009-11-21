6aa5df065b2579eaee4a7b2a1eec43fa0e9a3bcd
Axel.Simon <Axel.Simon@ens.fr>
2009-11-21 13:21:29 +0000

    Add a wrapper around the finalizer of Gtk objects.

--- gtk/Graphics/UI/Gtk/General/hsgthread.c
+++ gtk/Graphics/UI/Gtk/General/hsgthread.c
@@ -10,12 +10,38 @@
  */
 
 #include <glib/gthread.h>
+#include <gdk/gdk.h>
 
+static GStaticRecMutex gtk2hs_mutex;
+
+/* Lock the Gtk2Hs lock. */
+void gtk2hs_lock() {
+  g_static_rec_mutex_lock(&gtk2hs_mutex);
+}
+
+/* Unlock the Gtk2Hs lock. */
+void gtk2hs_unlock() {
+  g_static_rec_mutex_unlock(&gtk2hs_mutex);
+}
+
+/* Initialize the threads system of Gdk and Gtk. Furthermore, install the
+Gtk2Hs specific lock and unlock functions. */
 void gtk2hs_threads_initialise () {
   static int threads_initialised = 0;
 
   if (!threads_initialised) {
     threads_initialised = 1;
+    gdk_threads_set_lock_functions((GCallback) &gtk2hs_lock,
+                                   (GCallback) &gtk2hs_unlock );
     g_thread_init(NULL);
+    gdk_threads_init();
+    g_static_rec_mutex_init(&gtk2hs_mutex);
   }
+}
+
+/* Free an object within the Gtk2Hs lock. */
+void gtk2hs_g_object_unref_locked(gpointer object) {
+  g_static_rec_mutex_lock(&gtk2hs_mutex);
+  g_object_unref(object);
+  g_static_rec_mutex_unlock(&gtk2hs_mutex);
 }
--- gtk/Graphics/UI/Gtk/General/hsgthread.h
+++ gtk/Graphics/UI/Gtk/General/hsgthread.h
@@ -1,1 +1,20 @@
-int gtk2hs_threads_initialise ();
+#include <glib.h>
+
+#ifndef HSGTHREAD_H
+#define HSGTHREAD_H
+
+/* Lock the Gtk2Hs lock. */
+void gtk2hs_lock(void);
+
+/* Unlock the Gtk2Hs lock. */
+void gtk2hs_unlock(void);
+
+/* Initialize the threads system of Gdk and Gtk. Furthermore, install the
+Gtk2Hs specific lock and unlock functions. */
+int gtk2hs_threads_initialise (void);
+
+/* Free an object within the Gtk2Hs lock. */
+void gtk2hs_g_object_unref_locked(gpointer object);
+
+#endif HSGTHREAD_H
+
