0cd4d420175ade543e17768fd65734136d4c6da3
Axel Simon <Axel.Simon@ens.fr>
2009-11-22 12:08:04 +0000

    Add finialization from the Gtk+ main loop.
    This patch adds a replaces the default destroy function for any GObject that
    is created in Gtk+ or any libraries that use Gtk+. These objects are now
    finalized using an idle handler that is executed by the Gtk+ main loop.
    By being executed by the main loop, the finalizers are run from the same
    thread as the main loop and, hence, the objects that hold Xlib or Win32
    resources will now free these resources from the thread that normally
    calls into Xlib/Win32. This fixes a problem with the -threaded runtime
    of ghc in which finalization of objects could happen from other threads
    which would cause Xlib errors.

---
+++ gtk/Graphics/UI/Gtk/General/Threading.hs
@@ -0,0 +1,34 @@
+-- -*-haskell-*-
+--  GIMP Toolkit (GTK) General
+--
+--  Author : Axel Simon
+--
+--  Created: 9 May 2009
+--
+--  Copyright (C) 2009 Axel Simon
+--
+--  This library is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU Lesser General Public
+--  License as published by the Free Software Foundation; either
+--  version 2.1 of the License, or (at your option) any later version.
+--
+--  This library is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--  Lesser General Public License for more details.
+--
+-- |
+-- Maintainer  : gtk2hs-users@lists.sourceforge.net
+-- Stability   : provisional
+-- Portability : portable (depends on GHC)
+--
+-- Support for the threaded RTS of ghc.
+--
+module Graphics.UI.Gtk.General.Threading (
+  objectUnrefFromMainloop
+  ) where
+
+import System.Glib.FFI
+
+foreign import ccall unsafe "hsgthread.h &gtk2hs_g_object_unref_from_mainloop"
+  objectUnrefFromMainloop :: FinalizerPtr a
--- gtk/Graphics/UI/Gtk/General/hsgthread.c
+++ gtk/Graphics/UI/Gtk/General/hsgthread.c
@@ -7,41 +7,95 @@
  * do it in C land so the state survives :reload in GHCi. So there is the
  * danger in a mixed language program, of someone else initialising the
  * glib thread system and us not being aware of it. :-(
+ *
+ * Besides the interaction with ghci, we provide a variant of g_object_unref
+ * that is used in all objects of Gtk+ and those libraries that build on Gtk+.
+ * This variant enqueues the object to be finalized and adds an idle handler
+ * into the main loop of Gtk+ that will call the actual finalizers on the
+ * enqueued objects. The aim is to ensure that finalizers for objects that
+ * may hold Xlib or Win32 resources are only run from the thread that runs the
+ * main Gtk+ loop. If this is not ensured then bad things happen at least on
+ * Win32 since that API is making use of thread-local storage that is not
+ * present if the finalizers, that are run by the GC in a different thread,
+ * call back into Win32 without this thread-local storage.
  */
 
+#include <glib.h>
 #include <glib/gthread.h>
 #include <gdk/gdk.h>
 
-static GStaticRecMutex gtk2hs_mutex;
+#undef DEBUG
 
-/* Lock the Gtk2Hs lock. */
-void gtk2hs_lock() {
-  g_static_rec_mutex_lock(&gtk2hs_mutex);
-}
+static GStaticMutex gtk2hs_finalizer_mutex;
+static GSource* gtk2hs_finalizer_source;
+static guint gtk2hs_finalizer_id;
+static GArray* gtk2hs_finalizers;
 
-/* Unlock the Gtk2Hs lock. */
-void gtk2hs_unlock() {
-  g_static_rec_mutex_unlock(&gtk2hs_mutex);
-}
+gboolean gtk2hs_run_finalizers(gpointer data);
 
-/* Initialize the threads system of Gdk and Gtk. Furthermore, install the
-Gtk2Hs specific lock and unlock functions. */
+/* Initialize the threads system of Gdk and Gtk. */
 void gtk2hs_threads_initialise () {
   static int threads_initialised = 0;
 
   if (!threads_initialised) {
     threads_initialised = 1;
-    gdk_threads_set_lock_functions((GCallback) &gtk2hs_lock,
-                                   (GCallback) &gtk2hs_unlock );
     g_thread_init(NULL);
     gdk_threads_init();
-    g_static_rec_mutex_init(&gtk2hs_mutex);
   }
 }
 
 /* Free an object within the Gtk2Hs lock. */
-void gtk2hs_g_object_unref_locked(gpointer object) {
-  g_static_rec_mutex_lock(&gtk2hs_mutex);
-  g_object_unref(object);
-  g_static_rec_mutex_unlock(&gtk2hs_mutex);
+void gtk2hs_g_object_unref_from_mainloop(gpointer object) {
+  g_static_mutex_lock(&gtk2hs_finalizer_mutex);
+
+#ifdef DEBUG
+  printf("adding finalizer!\n");
+#endif
+
+  /* Ensure that the idle handler is still installed and that
+     the array of objects that are to be finalized exists. */
+  if (gtk2hs_finalizer_id==0) {
+
+    if (gtk2hs_finalizers == NULL)
+      gtk2hs_finalizers = g_array_new(0, 0, sizeof(gpointer));
+
+    if (gtk2hs_finalizer_source != NULL) {
+      g_source_destroy(gtk2hs_finalizer_source);
+      g_source_unref(gtk2hs_finalizer_source);
+    };
+
+    gtk2hs_finalizer_source = g_idle_source_new();
+    g_source_set_callback(gtk2hs_finalizer_source, &gtk2hs_run_finalizers, 0, 0);
+    gtk2hs_finalizer_id = g_source_attach(gtk2hs_finalizer_source, NULL);
+
+  };
+
+  /* Add the object to the list. */
+  g_array_append_val(gtk2hs_finalizers, object);
+
+  g_static_mutex_unlock(&gtk2hs_finalizer_mutex);
 }
+
+/* Run the finalizers that have been accumulated. */
+gboolean gtk2hs_run_finalizers(gpointer data) {
+  gint index;
+  g_assert(gtk2hs_finalizers!=NULL);
+
+  g_static_mutex_lock(&gtk2hs_finalizer_mutex);
+
+#ifdef DEBUG
+  printf("running %i finalizers!\n", gtk2hs_finalizers->len);
+#endif
+
+  for (index = 0; index < gtk2hs_finalizers->len; index++)
+    g_object_unref(g_array_index (gtk2hs_finalizers, GObject*, index));
+
+  g_array_set_size(gtk2hs_finalizers, 0);
+
+  gtk2hs_finalizer_id = 0;
+
+  g_static_mutex_unlock(&gtk2hs_finalizer_mutex);
+
+  return FALSE;
+}
+
