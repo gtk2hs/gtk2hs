8c62cbe1387d9995acbf7d0645912062ca98d577
Axel.Simon <Axel.Simon@in.tum.de>
2010-05-05 07:56:46 +0000

    Make the bootstrap file ignore a package if it can't be built.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,12 @@
 
 # A script to bootstrap gtk2hs
 
-for pkg in glib cairo pango gtk gconf gio glade gnomevfs gtkglext gtksourceview2 soegtk svgcairo vte webkit; do cd $pkg; cabal clean; cabal configure --user; cabal build; cabal haddock; cabal install; cd ..; done;
+for pkg in glib cairo pango gtk gconf gio glade gnomevfs gtkglext gtksourceview2 soegtk svgcairo vte webkit; do 
+  cd $pkg;
+  if cabal configure $1; then
+    cabal build;
+    cabal haddock;
+    cabal install;
+  fi;
+  cd ..;
+done;
