4a8e866dd188018522505220a1dd59a62cbd5bbb
Andy Stewart <lazycat.manatee@gmail.com>
2010-05-15 15:06:30 +0000

    Add gstreamer to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # A script to bootstrap gtk2hs
 
-for pkg in glib gio cairo pango gtk gconf glade gnomevfs gtkglext gtksourceview2 soegtk svgcairo vte webkit; do 
+for pkg in glib gio cairo pango gtk gconf glade gnomevfs gstreamer gtkglext gtksourceview2 soegtk svgcairo vte webkit; do 
   cd $pkg;
   if cabal configure $1; then
     cabal build;
