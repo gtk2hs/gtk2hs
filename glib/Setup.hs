a7bdbf7817f8f00be62646f995a44efdeb97ed1a
Andy Stewart <lazycat.manatee@gmail.com>
2010-09-24 00:13:36 +0000

    Update Gtk2HsSetup.hs in glib

--- glib/Setup.hs
+++ glib/Setup.hs
@@ -1,7 +1,9 @@
 -- Setup file for a Gtk2Hs module. Contains only adjustments specific to this module,
 -- all Gtk2Hs-specific boilerplate is stored in Gtk2HsSetup.hs which should be kept
 -- identical across all modules.
-import Gtk2HsSetup ( gtk2hsUserHooks )
+import Gtk2HsSetup ( gtk2hsUserHooks, checkGtk2hsBuildtools )
 import Distribution.Simple ( defaultMainWithHooks )
 
-main = defaultMainWithHooks gtk2hsUserHooks
+main = do
+  checkGtk2hsBuildtools ["gtk2hsC2hs"]
+  defaultMainWithHooks gtk2hsUserHooks
