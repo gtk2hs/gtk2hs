df5424b3a0e23ac2e168ab7fa7d1154135c5d770
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2013-10-15 18:49:17 +1300

    Clean gtk before building gtk3

--- install-both.sh
+++ install-both.sh
@@ -11,6 +11,8 @@ mv gtk/gtk.cabal-renamed gtk/gtk.cabal || true
 mv gtk/gtk3.cabal gtk/gtk3.cabal-renamed || true
 cabal install ./gtk --reinstall --force-reinstall;
 
+rm -rf gtk/dist
+
 mv gtk/gtk3.cabal-renamed gtk/gtk3.cabal || true
 mv gtk/gtk.cabal gtk/gtk.cabal-renamed || true
 cabal install ./gtk --reinstall --force-reinstall;
