c688c8648be2cc8d4c3bae42a4b46c9c2c73bb7c
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2014-04-15 16:22:17 +1200

    Add GHC 7.8.2 to travis build

--- install-both.sh
+++ install-both.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -ex
 
 # Install both gtk and gtk3
-cabal install ./tools --reinstall --force-reinstall;
-cabal install ./glib --reinstall --force-reinstall;
-cabal install ./gio --reinstall --force-reinstall;
-cabal install ./cairo --reinstall --force-reinstall;
-cabal install ./pango --reinstall --force-reinstall;
+$CABAL install ./tools --reinstall --force-reinstall;
+$CABAL install ./glib --reinstall --force-reinstall;
+$CABAL install ./gio --reinstall --force-reinstall;
+$CABAL install ./cairo --reinstall --force-reinstall;
+$CABAL install ./pango --reinstall --force-reinstall;
 
 mv gtk/gtk.cabal-renamed gtk/gtk.cabal || true
 mv gtk/gtk3.cabal gtk/gtk3.cabal-renamed || true
-cabal install ./gtk --reinstall --force-reinstall;
+$CABAL install ./gtk --reinstall --force-reinstall;
 
 rm -rf gtk/dist
 
 mv gtk/gtk3.cabal-renamed gtk/gtk3.cabal || true
 mv gtk/gtk.cabal gtk/gtk.cabal-renamed || true
-cabal install ./gtk -fbuild-demos --reinstall --force-reinstall;
+$CABAL install ./gtk -fbuild-demos --reinstall --force-reinstall;
 
