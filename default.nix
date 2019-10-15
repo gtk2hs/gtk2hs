9c52bc10df297a7f6111a4c9a50cd24e0ccac208
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-10-16 00:47:44 +1300

    Use nixpkgs that has fix needed for GI on macOS

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@
       sha256 = "0jjis8y1p3wj45v72alhcnrcxg1f2p6r6bi0383qwxlmngf3cizz";
     }))
 , nixpkgs ? builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/393e96b3fac51139c134209c5d4f2738506b140e.tar.gz";
-    sha256 = "0mlgwmrqd4734f2fjxqm44jxpr78qm7yy3kzh1zynd5mn9fpqw0b";
+    url = "https://github.com/NixOS/nixpkgs/archive/f91f506680077924e5a1f0c861d10e946a5c7f8d.tar.gz";
+    sha256 = "0bmx8ig3r8r5qskyyn7bzjfvz38k19yvhjfz3lkcglpqlcbmah4p";
   }
 , haskellCompiler ? "ghc865"
 }:
