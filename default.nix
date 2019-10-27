13258345dbb92830bc3b284655ae02f4f7e5c64c
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-10-27 19:22:49 +1300

    Update haskell.nix and nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
-{ pkgs ? import nixpkgs (import (builtins.fetchTarball {
-      url = "https://github.com/input-output-hk/haskell.nix/archive/3bdc2a068498baac48f3be296a611f750033d551.tar.gz";
-      sha256 = "0jjis8y1p3wj45v72alhcnrcxg1f2p6r6bi0383qwxlmngf3cizz";
-    }))
+{ pkgs ? import nixpkgs (haskellNixpkgsArgs // {
+    overlays = haskellNixpkgsArgs.overlays ++ [ (import ./nix/overlays/gtk-debug.nix) ];
+  } // (if system == null then {} else { inherit system; }))
 , nixpkgs ? builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f91f506680077924e5a1f0c861d10e946a5c7f8d.tar.gz";
-    sha256 = "0bmx8ig3r8r5qskyyn7bzjfvz38k19yvhjfz3lkcglpqlcbmah4p";
+    url = "https://github.com/input-output-hk/nixpkgs/archive/d08a74315610096187a4e9da5998ef10e80de370.tar.gz";
+    sha256 = "16i3n5p4h86aswj7y7accmkkgrrkc0xvgy7fl7d3bsv955rc5900";
   }
+, haskellNixpkgsArgs ? import (builtins.fetchTarball {
+    url = "https://github.com/input-output-hk/haskell.nix/archive/4a24da99c1c9783414372bdd0686f79f4e60bfe9.tar.gz";
+    sha256 = "10w8mixdkqd194i68h22y11nl46imnc26f8xgxra33slr040paxb";
+  })
 , haskellCompiler ? "ghc865"
+, system ? null
 }:
   pkgs.haskell-nix.cabalProject {
     src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
