a99b43f3503c5f046ec0b9d3190857e09d07a6a9
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-03-24 20:54:56 +1300

    Update version bounds for Cabal 3.6

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,2 +1,7 @@
-{ overlays = [ (import ./overlays/gtk-debug.nix) ];
+{ compiler-nix-name = "ghc922";
+  overlays = [ (import ./overlays/gtk-debug.nix) ];
+  shell.tools.cabal = {};
+  shell.withHoogle = false;
+  # shell.tools.hoogle = {};
+  modules = [{ nonReinstallablePkgs = ["rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base" "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell" "bytestring" "ghc-bignum"]; }];
 }
