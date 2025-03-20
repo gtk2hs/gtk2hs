c4d18840137bdf5417b05371f125e5c2421ca168
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-03-20 20:58:28 +1300

    Update CI

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,10 +1,11 @@
-{pkgs, ...}: { compiler-nix-name = "ghc9101";
-  flake.variants.ghc8107.compiler-nix-name = pkgs.lib.mkForce "ghc8107";
-  flake.variants.ghc966.compiler-nix-name = pkgs.lib.mkForce "ghc966";
-  flake.variants.ghc982.compiler-nix-name = pkgs.lib.mkForce "ghc982";
+{pkgs, ...}: { compiler-nix-name = "ghc912";
+  flake.variants.ghc810.compiler-nix-name = pkgs.lib.mkForce "ghc810";
+  flake.variants.ghc96.compiler-nix-name = pkgs.lib.mkForce "ghc96";
+  flake.variants.ghc98.compiler-nix-name = pkgs.lib.mkForce "ghc98";
+  flake.variants.ghc910.compiler-nix-name = pkgs.lib.mkForce "ghc910";
   overlays = [ (import ./overlays/gtk-debug.nix) ];
   shell.tools.cabal = {};
-  shell.tools.haskell-ci.src = pkgs.inputs.haskell-ci;
+  shell.tools.haskell-ci = { compiler-nix-name = "ghc910"; src = pkgs.inputs.haskell-ci; };
   shell.withHoogle = false;
   # shell.tools.hoogle = {};
 }
